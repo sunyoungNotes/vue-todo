63176c7f7625fcfca7a3e695391aaea645ffd855
sunyoungNotes <sunyoungNotes@gmail.com>
2022-10-08 00:02:21 +0900

    프로젝트 생성

---
+++ src/components/TodoList.vue
@@ -0,0 +1,71 @@
+<template>
+    <section>
+        <ul>
+            <!-- <li>할일 1</li>
+            <li>할일 2</li>
+            <li>할일 3</li> -->
+            <li v-for="(todoItem, index) in propsdata" :key="todoItem" class="shadow">
+                <i class="checkBtn fas fa-check" aria-hidden="true"></i>
+                {{ todoItem }}
+                <span class="removeBtn" type="button" @click="removeTodo(todoItem, index)">
+                    <i class="far fa-trash-alt" aria-hidden="true"></i>
+                </span>
+            </li>
+        </ul>
+    </section>
+</template>
+
+<script>
+export default {
+    props: ['propsdata'],
+    // data() {
+    //     return {
+    //         todoItems: []
+    //     }
+    // },
+    // created() {
+    //     if (localStorage.length > 0) {
+    //         for (var i = 0; i < localStorage.length; i++) {
+    //             this.todoItems.push(localStorage.key(i));
+    //         }
+    //     }
+    // },
+    methods: {
+        removeTodo(todoItem, index) {
+            // console.log('clicked');
+            // console.log(this.todoItems, index);
+            // localStorage.removeItem(todoItem);
+            // this.todoItems.splice(index, 1);
+            this.$emit('removeTodo', todoItem, index);
+        }
+    }
+}
+</script>
+
+<style scoped>
+    ul {
+        list-style-type: none;
+        padding-left: 0px;
+        margin-top: 0;
+        text-align: left;
+    }
+    li {
+        display: flex;
+        min-height: 50px;
+        height: 50px;
+        line-height: 50px;
+        margin: 0.5rem 0;
+        padding: 0 0.9rem;
+        background: white;
+        border-radius: 5px;
+    }
+    .checkBtn {
+        line-height: 45px;
+        color: #62acde;
+        margin-right: 5px;
+    }
+    .removeBtn {
+        margin-left: auto;
+        color: #de4343;
+    }
+</style>
